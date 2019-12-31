ae50111ce2cea02b0050eb6706255f11b32ac70b
Manjunathac <mjnathachandrappa@gmail.com>
2019-12-31 07:48:54 +0530

    deleted

--- Dp6_Devskil/booksapp/books/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class BooksConfig(AppConfig):
-    name = 'booksapp.books'
-    verbose_name = 'Books'
-
-    def ready(self):
-        pass
--- Dp6_Devskil/envn6/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\mr\myprojects\dp6_devskil\envn6\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
